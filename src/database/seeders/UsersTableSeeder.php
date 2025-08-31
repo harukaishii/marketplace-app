cc884980c54a9479b08e10a463c1b22b7bc07de1
haruka ishii <harukaishii.99@gmail.com>
2025-08-31 12:37:25 +0900

    seeder修正

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\support\Facades\Hash;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Faker\Factory as Faker;
 
